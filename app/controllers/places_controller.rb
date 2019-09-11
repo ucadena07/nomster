ed780b0efab2491b73fa5b8b01b1615ae69c196b
Ulises Cadena <ucadena07@gmail.com>
2019-09-11 14:08:19 +0000

    Handle form submission

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,13 +1,24 @@
 class PlacesController < ApplicationController
 
 def index
-  @places = Place.paginate(page: params[:page], per_page: 1)
+  @places = Place.all.paginate(page: params[:page], per_page: 10)
 end
 
 def new
   @place = Place.new 
 end 
 
+def create
+  Place.create(place_params)
+  redirect_to root_path
+end 
+
+private 
+
+def place_params
+  params.require(:place).permit(:name, :description, :address)
+
+end 
 
 
 
