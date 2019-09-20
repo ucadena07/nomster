22adbcb70645c654b512085064b6762979da321b
Ulises Cadena <ucadena07@gmail.com>
2019-09-20 15:07:30 +0000

    form submission

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
-  resources :places
+  resources :places do 
+    resources :places 
+      resources :comments, only: :create 
+    end 
 end
